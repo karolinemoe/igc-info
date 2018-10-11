562ecec8ffba74a716e40e08211cb11751b58094
karoline <karolmar@stud.ntnu.no>
2018-10-11 17:57:21 +0200

    uptime

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	//"time"
+	"time"
 	"net/http"
     "google.golang.org/appengine"
-	//"github.com/gorilla/mux"
 )
+
+// store boot time
+var startTime = time.Now()
 
 func main() {
 
@@ -25,12 +27,14 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprint(w, "API RESPONSE")
 
 	type Info struct {
+		Uptime string `json:"time"`
 		Info string `json:"info"`
 		Version string `json:"version"`
 	}
 
 	info := &Info{
-		Info: "IGC Info API",
+		Uptime: uptime(),
+		Info: "Service for IGC tracks.",
 		Version: "v1",
 	}
 
@@ -39,6 +43,30 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(string(i))
 	fmt.Fprint(w, string(i))
 
+}
+
+func uptime() string{
+	// calculate time since boot in UNIX time (seconds) and convert that into a time.Time object
+	uptime := time.Now().Unix() - startTime.Unix()
+	ut := time.Unix(uptime, 0)
+
+	// calculate each component of the ISO8601 format
+	years, months, days 	:= ut.Date()
+	hours, minutes, seconds := ut.Clock()
+
+	// subtract 1 as all three is not 0-indexed
+	months, hours, days = months-1, hours-1, days-1
+
+	// construct final string with all components
+	return fmt.Sprintf("%s%d%s%d%s%d%s%d%s%d%s%d%s", "P", absInt(int64(years - 1970)), "Y", months, "M", days, "DT", hours, "H", minutes, "M", seconds, "S")
+}
+
+// take absolute value of n
+func absInt(n int64) int64 {
+	if n < 0 {
+		return -n
+	}
+	return n
 }
 
 func igcHandler(w http.ResponseWriter, r *http.Request) {
