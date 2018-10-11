5dd9e7666a402014e00a804ae357837a1ed799c6
karoline <karolmar@stud.ntnu.no>
2018-10-11 11:37:57 +0200

    testing

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ package main
 import (
     "fmt"
 	"net/http"
+    "google.golang.org/appengine"
 )
 func main() {
         http.HandleFunc("/", handle)
+        appengine.Main()
 }
 func handle(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintln(w, "Hello, Application!")
