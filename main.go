ed2486f864c19d2f905a97fee8e4e6beba011a3d
karoline <karolmar@stud.ntnu.no>
2018-10-11 11:41:39 +0200

    goigc usage from marni repo

--- main.go
+++ main.go
@@ -6,10 +6,22 @@ import (
     "fmt"
 	"net/http"
     "google.golang.org/appengine"
+	"github.com/marni/goigc"
+
 )
 func main() {
         http.HandleFunc("/", handle)
         appengine.Main()
+
+
+	s := "http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc"
+	track, err := igc.ParseLocation(s)
+	if err != nil {
+		fmt.Errorf("Problem reading the track", err)
+	}
+
+	fmt.Printf("Pilot: %s, gliderType: %s, date: %s",
+		track.Pilot, track.GliderType, track.Date.String())
 }
 func handle(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintln(w, "Hello, Application!")
