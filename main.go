7e75d142e53df6b8e6ddd087ea0f0c46cf7b1b81
karoline <karolmar@stud.ntnu.no>
2018-10-11 14:17:41 +0200

    test

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ package main
 
 import (
     "fmt"
-    "encoding/json"
 	//"time"
 	"net/http"
     "google.golang.org/appengine"
@@ -16,31 +15,13 @@ func main() {
 	root := "/igcinfo"
 	router := mux.NewRouter()
 
-	//http.HandleFunc(root, homeHandler)
-	router.HandleFunc(root + "/api", apiHandler)
+	router.HandleFunc(root+"/api", apiHandler)
 
 	appengine.Main()
 
 }
 
-func apiHandler(w http.ResponseWriter, r *http.Request){
-	// API info object schema
-	type APIInfo struct {
-		//Uptime  string `json:"uptime"`
-		Info    string `json:"info"`
-		Version string `json:"version"`
-		//BootTime string `json:"boot_time"`
-	}
-
-	// construct the current response
-	info := APIInfo{
-		//Uptime:  uptimeFormat(),
-		//BootTime: startTime.UTC().Format(time.RFC3339),
-		Info:    "Service for IGC tracks",
-		Version: "v1",
-	}
-
-	json.NewEncoder(w).Encode(info)
+func apiHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprint(w, "API RESPONS")
 }
