6fbf4ba689f5c0337f353bef261c9a99da77180a
karoline <karolmar@stud.ntnu.no>
2018-10-11 13:07:06 +0200

    clean

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ package main
 import (
     "fmt"
 	"net/http"
+    "google.golang.org/appengine"
 
 )
 func main() {
         http.HandleFunc("/", handle)
+        appengine.Main()
 
 }
 func handle(w http.ResponseWriter, r *http.Request) {
